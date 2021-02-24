4596aacebedd4b12a531d35a1bd20caff1b96ea7
picassonk21 <picassonk21@gmail.com>
2021-02-24 19:25:43 +0300

    init: start human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
-module.exports = function toReadable (number) {
-  
+const units = {
+    '0': 'zero',
+    '1': 'one',
+    '2': 'two',
+    '3': 'three',
+    '4': 'four',
+    '5': 'five',
+    '6': 'six',
+    '7': 'seven',
+    '8': 'eight',
+    '9': 'nine',
+    '10': 'ten',
+    '11': 'eleven',
+    '12': 'twelve',
+    '13': 'thirteen',
+    '14': 'fourteen',
+    '15': 'fifteen',
+    '16': 'sixteen',
+    '17': 'seventeen',
+    '18': 'eighteen',
+    '19': 'nineteen'
+}
+
+const dozens = {
+    '20': 'twenty',
+    '30': 'thirty',
+    '40': 'forty',
+    '50': 'fifty',
+    '60': 'sixty',
+    '70': 'seventy',
+    '80': 'eighty',
+    '90': 'ninety'
+}
+
+module.exports = function toReadable(number) {
+    if (number < 20) {
+        return `${units[number]}`
+    } else if (number < 100) {
+        let unit = '';
+        +(String(number)[1]) ? unit = ' ' + units[String(number)[1]] : unit = ''
+        return `${dozens[String((String(number)[0]) * 10)]}${unit}`
+    } else {
+        let unit = +(String(number).slice(1))
+        if (unit > 0) {
+            unit = ' ' + toReadable(unit)
+        } else {
+            unit = ''
+        }
+        return `${units[String(number)[0]]} hundred${unit}`
+    }
 }
